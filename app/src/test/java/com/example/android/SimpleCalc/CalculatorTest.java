598c9ab16d370c481d12ab634746384ed8438fda
Bulana <Bulana@hal9000.zapper.local>
2019-05-22 16:39:24 +0200

    added unit tests

--- app/src/test/java/com/example/android/SimpleCalc/CalculatorTest.java
+++ app/src/test/java/com/example/android/SimpleCalc/CalculatorTest.java
@@ -25,7 +25,9 @@ import org.junit.runners.JUnit4;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.number.IsCloseTo.closeTo;
 import static org.junit.Assert.assertThat;
+
 
 /**
  * JUnit4 unit tests for the calculator logic. These are local unit tests; no device needed
@@ -53,6 +55,39 @@ public class CalculatorTest {
         assertThat(resultAdd, is(equalTo(2d)));
     }
 
-
-
+    @Test
+    public void addTwoNumbersNegative() {
+        double resultAdd = mCalculator.add(-1d, 2d);
+        assertThat(resultAdd, is(equalTo(1d)));
+    }
+    @Test
+    public void addTwoNumbersFloats() {
+        double resultAdd = mCalculator.add(1.111f, 1.111d);
+        assertThat(resultAdd, is(closeTo(2.222, 0.01)));
+    }
+    @Test
+    public void subTwoNumbers() {
+        double resultSub = mCalculator.sub(1d, 1d);
+        assertThat(resultSub, is(equalTo(0d)));
+    }
+    @Test
+    public void subWorksWithNegativeResult() {
+        double resultSub = mCalculator.sub(1d, 3d);
+        assertThat(resultSub, is(equalTo(-2d)));
+    }
+    @Test
+    public void mulTwoNumbers() {
+        double resultMul = mCalculator.mul(5d, 2d);
+        assertThat(resultMul, is(equalTo(10d)));
+    }
+    @Test
+    public void divTwoNumbers() {
+        double resultDiv = mCalculator.div(4d,4d);
+        assertThat(resultDiv, is(equalTo(1d)));
+    }
+    @Test
+    public void divTwoNumbersZero() {
+        double resultDiv = mCalculator.div(6d,0);
+        assertThat(resultDiv, is(equalTo(Double.POSITIVE_INFINITY)));
+    }
 }
